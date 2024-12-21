407e621b9ea10c78b7531d8bd0c5ef32b8d62239
Rahul Mishra <rahulmishra4317@gmail.com>
2024-12-22 00:39:32 +0530

    how to send a request in body

--- main.py
+++ main.py
@@ -1,7 +1,43 @@
-from fastapi import FastAPI
+from fastapi import FastAPI 
+from fastapi.params import Body
+
+
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/landingpage")
 def read_root():
     return {"Hello": "World"}
+
+@app.post("/hello_user")
+def user_greeting():
+    return {"message_for_user" : "Hello user how are you!"}
+
+@app.post("/create_post")
+async def create_post(payload : dict = Body(...)):
+    print("Payload" , payload)
+    return {"message" : "Rahul Mishra"}
+
+
+@app.post("/create_post_with_body") 
+async def create_post_with_body(payload : dict = Body(...)):
+    print("payload ----" , payload)
+    print("type of payload" , type(payload))
+    title = payload['title']
+    content = payload.get("content")
+
+    print("Title " , title)
+    print("content" , content)
+    return {"message" : "Post created successfully"}
+
+
+
+@app.post("/greet_user")
+async def greet_user(payload : dict = Body(...)):
+    print(f"Payload -> {payload}")
+    name = payload.get("name")
+    age = payload.get("age")
+    gender = payload["gender"]
+    print("The name , age and gender" , name , age , gender)
+    message = f"Hello {name} you are {age} years old and your gender is {gender}" 
+    return {"greeting" : message}
