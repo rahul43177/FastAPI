6afe81244c085b6e298120a000d46e1ca4098182
Rahul <rahulmishra4317@gmail.com>
2025-02-17 15:15:12 +0530

    Python refresher:FastAPI course

--- python-basics/listAssignment.py
+++ python-basics/listAssignment.py
@@ -1,16 +1,10 @@
 """
 Create a list of 5 animals called zoo
-
 - Delete the animal at the 3rd index.
-
 - Append a new animal at the end of the list
-
 - Delete the animal at the beginning of the list.
-
 - Print all the animals
-
 - Print only the first 3 animals
-
 """
 
 animals = ["Zebra" , "Dog" , "Lion" , "Cow" , "Cat"]
@@ -22,7 +16,6 @@ animals.append("Penguin")
 print(f"The new list post appending {animals}")
 del animals[0]
 print(f"the new list post deleting the first element -> {animals}")
-
 print("\n")
 print(f"Printing the first three elements {animals[0:3]}")
 
