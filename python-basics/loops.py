e2adc9434f41a2b212dd3e3b0b5eafed3f3b73b8
Rahul <rahulmishra4317@gmail.com>
2025-02-23 22:30:18 +0530

    Python while loops

--- python-basics/loops.py
+++ python-basics/loops.py
@@ -1,16 +1,14 @@
-my_list = [1,2,3,4,5]
+# For loop example iterating through a list
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(f"Current fruit: {fruit}")
 
-for i in range(len(my_list)):
-    print(my_list[i])
+print("\n")  # Adding space between examples
 
+# While loop countdown example
+count = 5
+while count > 0:
+    print(f"Countdown: {count}")
+    count -= 1
 
-
-sum = 0
-for i in range(5):
-    sum += my_list[i]
-new_sum = 0
-for val in my_list:
-    new_sum += val
-
-print(f"The sum of the for loop is -> {sum}")
-print(f"The new sum is {new_sum}")
+print("Blastoff!")
