88eaea099107a4031cbb9bec4ce1304b3c73ee2d
Rahul <rahulmishra4317@gmail.com>
2025-02-23 23:54:52 +0530

    OOPS : Learning

--- python-basics/OOPS-pillar/main.py
+++ python-basics/OOPS-pillar/main.py
@@ -9,6 +9,9 @@ enemy2.type_of_enemy = "Terrorist"
 enemy1.attack_damage = 12 
 enemy2.attack_damage = 20
 
+
+
+
 print(f"{enemy1.type_of_enemy} has {enemy1.health_point} health points and {enemy1.attack_damage} attack damage")
 print(f"{enemy2.type_of_enemy} has {enemy2.health_point} health points and {enemy2.attack_damage} attack damage")
 
