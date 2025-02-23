b7ed51b4aaa529bb1ea724a4655b5f061857b325
Rahul <rahulmishra4317@gmail.com>
2025-02-23 23:42:16 +0530

    OOPS : Learning

--- python-basics/OOPS-pillar/main.py
+++ python-basics/OOPS-pillar/main.py
@@ -11,3 +11,13 @@ enemy2.attack_damage = 20
 
 print(f"{enemy1.type_of_enemy} has {enemy1.health_point} health points and {enemy1.attack_damage} attack damage")
 print(f"{enemy2.type_of_enemy} has {enemy2.health_point} health points and {enemy2.attack_damage} attack damage")
+
+enemy1.walk_forward()
+enemy2.walk_forward()
+enemy1.attack()
+enemy2.attack()
+
+enemy1.talk()
+enemy2.talk()
+
+    
