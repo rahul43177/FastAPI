7e3570c6f1dfeb20a4d03bef7f77ab90c9aac067
Rahul <rahulmishra4317@gmail.com>
2025-04-28 15:30:55 +0530

    FastAPI setup with poetry

--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from routers.itemRouter import router as item_router
-from routers.userRouter import router as user_router
-
-app = FastAPI()
-
-app.include_router(user_router , prefix='/api' , tags=['USERS'])
-app.include_router(item_router , prefix='/api' , tags = ['ITEMS'])
-
