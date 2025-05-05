c4e14ecd38b5de1f290ab2a8d0ca7f0481a91b53
Rahul <rahulmishra4317@gmail.com>
2025-05-05 13:10:14 +0530

    FastAPI : Put Method

--- fastapi_learning/main.py
+++ fastapi_learning/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , Body
 from pydantic import BaseModel , field_validator
 app = FastAPI()
 
@@ -82,110 +82,12 @@ BOOKS_DATABASE = [
     }
 ]
 
-
-CARS_DATABASE = [
-    {
-        "id": 1,
-        "make": "Toyota",
-        "model": "Camry",
-        "year": 2022,
-        "color": "Silver",
-        "price": 25000
-    },
-    {
-        "id": 2,
-        "make": "Honda",
-        "model": "Civic",
-        "year": 2023,
-        "color": "Blue",
-        "price": 22000
-    },
-    {
-        "id": 3,
-        "make": "Tesla",
-        "model": "Model 3",
-        "year": 2023,
-        "color": "White",
-        "price": 45000
-    },
-    {
-        "id": 4,
-        "make": "Ford",
-        "model": "Mustang",
-        "year": 2022,
-        "color": "Red",
-        "price": 35000
-    },
-    {
-        "id": 5,
-        "make": "BMW",
-        "model": "X5",
-        "year": 2023,
-        "color": "Black",
-        "price": 65000
-    }
-]
-
-
-@app.get("/getallbooks")
-def get_all_books():
+@app.put("/books/update_book")
+async def update_book(updated_book = Body()):
+    for i in range(len(BOOKS_DATABASE)):
+        if BOOKS_DATABASE[i].get("title").casefold() == updated_book.get("title").casefold():
+            BOOKS_DATABASE[i] = updated_book
+            
+@app.get("/books/allbook")
+async def get_all_book():
     return BOOKS_DATABASE
-
-# add a new book to database 
-class AddBook(BaseModel):
-    id : int 
-    title : str 
-    author : str 
-    year : int 
-    genre : str
-
-    @field_validator("id")
-    def validate_id(cls , v) :
-        if v < 0:
-            return {"message" : "THe id can't be negative"}
-        if isinstance(v , str):
-            return {"mssage" : "The id can't be a string"}
-        return v 
-        
-
-@app.post("/addbook")
-def add_new_book(single_book : AddBook):
-    BOOKS_DATABASE.append(single_book)
-    return {
-        "message" : "The data has been added" , 
-        "book data" : BOOKS_DATABASE
-    }
-    
-
-#i want a add car to database API
-class AddCar(BaseModel):
-    id : int 
-    make : str 
-    model : str 
-    year : int 
-    color : str 
-    price : int 
-
-@app.post("/addcar")
-def add_new_car(single_car : AddCar):
-    CARS_DATABASE.append(single_car)
-    return {
-        "message" : "The data has been added" , 
-        "car data" : CARS_DATABASE
-    }
-
-
-#i want a get all cars from database API
-@app.get("/getallcars")
-def get_all_cars():
-    return CARS_DATABASE
-
-
-#i want a get a single car from database API
-@app.get("/getcar/{car_id}")
-def get_single_car(car_id : int):
-    for car in CARS_DATABASE:
-        if car["id"] == car_id:
-            return car
-    return {"message" : "The car is not found"} 
-        
